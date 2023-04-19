a9670c3b4fb785a5053eefe5f5f4432d95e1ab3b
ㅋ <128570436+nanunbabohaha@users.noreply.github.com>
2023-04-19 15:41:27 +0900

    nanunbabohaha

--- nanunbabohaha/bin/nanunbabohaha.dart
+++ nanunbabohaha/bin/nanunbabohaha.dart
@@ -51,19 +51,5 @@ void main() {
   for (int a = 0; a < 10; a++) {
     print(a * b);
   }
-
-bool a(int a){
-if (a ==1) {
-     return false;
 }
-for (int i = 2; i<a; i++){
-if(a % i == 0){
-}
-}
-}
- }
 
-
-
-
-  }
