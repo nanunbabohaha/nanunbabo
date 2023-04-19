87ef4b389ec926b9112194bef4d2ceb58d209068
ㅋ <128570436+nanunbabohaha@users.noreply.github.com>
2023-04-19 15:40:37 +0900

    nanunbabo

--- nanunbabohaha/bin/nanunbabohaha.dart
+++ nanunbabohaha/bin/nanunbabohaha.dart
@@ -1,21 +1,69 @@
-import 'package:nanunbabohaha/nanunbabohaha.dart' as nanunbabohaha;
-
 void main() {
- var x= 10;
- var y = -3;
+//for반복문ㅁ
+
+  /* for (int i = 0; i < 10; i++) {
+    print(i);
+  }
+*/
+  //while 반복문
+
+  /*int i = 0;
+
+  while(i<10){
+    print(i);
+    i++;
+  }*/
+
+  //함수
+
+  /*void f() {
+    print("1");
+    print("f");
+  }
+  for (int i = 0; i < 10; i++) {
+    f();
+  }*/
+
+  /* void f() {
+    print("1");
+  }
+  for (int i = 0; i <= 100; i++) {
+  print(i);
+  }*/
+
+  //변환값이 있는 함수
+  /*int g(int x){
+    return x * x;
+  }
 
 
- if(x > 0 && y > 0){
+  int n = g(10);
+  print(n);
+*/
+  /* int g(int x,int y){
+    return x + y;
+  }
+  int n = g(10,2);
+  print(n); */
 
-  print("1");}
- else if(x < 0 && y > 0){
-   print("2");}
- else if(x < 0 && y < 0){
-   print("3");}
- else if(x > 0 && y < 0){
-   print("4");}
- else{
-   print("아무 사분면에도 존재하지 않는다");
- }
-nanunbabohaha.
+  int b = 2;
+
+  for (int a = 0; a < 10; a++) {
+    print(a * b);
+  }
+
+bool a(int a){
+if (a ==1) {
+     return false;
 }
+for (int i = 2; i<a; i++){
+if(a % i == 0){
+}
+}
+}
+ }
+
+
+
+
+  }
---
+++ nanunbabohaha/lib/sffqf.dart
@@ -0,0 +1,23 @@
+/*
+import 'package:nanunbabohaha/nanunbabohaha.dart' as nanunbabohaha;
+
+void main() {
+  var x= 10;
+  var y = -3;
+
+
+  if(x > 0 && y > 0){
+
+    print("1");}
+  else if(x < 0 && y > 0){
+    print("2");}
+  else if(x < 0 && y < 0){
+    print("3");}
+  else if(x > 0 && y < 0){
+    print("4");}
+  else{
+    print("아무 사분면에도 존재하지 않는다");
+  }
+  nanunbabohaha.
+}
+*/
